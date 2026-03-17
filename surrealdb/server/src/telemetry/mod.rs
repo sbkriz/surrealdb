ee3a7dcf1169ad32b54f9a7bade32261b787a72a
Emmanuel Keller <emmanuel.keller@surrealdb.com>
2026-03-17 19:45:49 -0300

    Fmt

--- surrealdb/server/src/telemetry/mod.rs
+++ surrealdb/server/src/telemetry/mod.rs
@@ -20,8 +20,7 @@ use tracing_subscriber::prelude::*;
 use tracing_subscriber::registry::LookupSpan;
 
 use crate::cli::LogFormat;
-pub use crate::cli::validator::parser::tracing::CustomFilter;
-pub use crate::cli::validator::parser::tracing::SpanFilter;
+pub use crate::cli::validator::parser::tracing::{CustomFilter, SpanFilter};
 use crate::cnf::ENABLE_TOKIO_CONSOLE;
 
 /// Identifies the logging destination that a filter is being configured for.
